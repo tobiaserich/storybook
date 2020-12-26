fe844aa6b01d10cf290dd81b3a3346bba702efe9
tobiaserich <tobiaserich@hotmail.de>
2020-12-26 19:50:17 +0100

    Delete example files and css

---
+++ src/components/Switch.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+export type SwitchPanelProps = {
+  shadowColor: string;
+};
+
+export type SwitchKnobProps = {
+  knobColor: string;
+  animation: boolean;
+};
+
+export type SwitchProps = {
+  shadowColor: SwitchPanelProps["shadowColor"];
+  knobColor: SwitchKnobProps["knobColor"];
+  animationTrigger: boolean;
+};
+
+export const SwitchPanel = styled("div")<SwitchPanelProps>`
+  display: flex;
+  align-items: center;
+  height: 10px;
+  max-width: 30px;
+  margin-left: 4px;
+  margin-right: 4px;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: inset 0px 2px 4px ${({ shadowColor }) => shadowColor};
+`;
+
+export const SwitchKnob = styled("div")<SwitchKnobProps>`
+  width: 15px;
+  height: 15px;
+  padding: 0;
+  margin: 0;
+  position: relative;
+  background-color: ${({ knobColor }) => knobColor};
+  box-shadow: inset 2px 2px 4px rgba(255, 255, 255, 0.4),
+    inset -2px -2px 4px rgba(0, 0, 0, 0.4);
+  border-radius: 50%;
+  border: none;
+  animation: ${({ animation }) =>
+    animation === null
+      ? ""
+      : animation
+      ? "switch-back 0.1s linear normal forwards"
+      : "switch 0.1s linear normal forwards"};
+
+  @keyframes switch {
+    0% {
+      left: 0;
+    }
+    100% {
+      left: calc(100% - 15px);
+    }
+  }
+  @keyframes switch-back {
+    0% {
+      left: calc(100% - 15px);
+    }
+    100% {
+      left: 0;
+    }
+  }
+`;
+
+export const SwitchButton: React.FC<SwitchProps> = ({
+  shadowColor,
+  knobColor,
+  animationTrigger,
+}) => {
+  const [animation, setAnimation] = React.useState(null);
+
+  React.useEffect(() => {
+    setAnimation(animationTrigger);
+  }, [animationTrigger]);
+  return (
+    <SwitchPanel shadowColor={shadowColor}>
+      <SwitchKnob
+        onClick={() => setAnimation(!animation)}
+        animation={animation}
+        knobColor={knobColor}
+      />
+    </SwitchPanel>
+  );
+};
