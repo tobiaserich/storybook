30cd7c57c65a9861e4d67e218d329f34af08d24a
tobiaserich <tobiaserich@hotmail.de>
2021-07-26 09:26:24 +0200

    Add new background color

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,13 @@
 export const parameters = {
+  backgrounds: {
+    default: "white",
+    values: [
+      {
+        name: "white",
+        value: "#ffff",
+      },
+      { name: "neumorphism", value: "#ffebd6" },
+    ],
+  },
   actions: { argTypesRegex: "^on[A-Z].*" },
 };
